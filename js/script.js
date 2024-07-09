2bfbe26eb448421784aec89b8040705ff1351519
Resul Elezi <resul_elezi@protonmail.com>
2024-07-09 12:43:01 +0200

    #

--- js/script.js
+++ js/script.js
@@ -41,6 +41,14 @@ function showResults(userOption) {
     computerScoreSpanElement.innerText = computerScore;
     playerScoreSpanElement.innerText = playerScore;
   
+    if (playerScore === 3 || computerScore === 3) {
+      winnerMsgElement.innerText = `${
+        playerScore === 3 ? "Player" : "Computer"
+      } has won the game!`;
+  
+      resetGameBtn.style.display = "block";
+      optionsContainer.style.display = "none";
+    }
   
   };
 
