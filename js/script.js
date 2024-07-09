e6d29f2dcb99605f1a52ebc9af8f74b37ab9623b
Resul Elezi <resul_elezi@protonmail.com>
2024-07-09 12:06:47 +0200

    #

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+  }
+
+let playerScore = 0;
+let computerScore = 0;
+  
+function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+  }
